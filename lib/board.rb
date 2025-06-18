b8716b88f723a556e0033d03bac0aae9e9372673
Sabin Thapa <sabin.thapa@bajratechnologies.com>
2025-06-18 06:39:03 +0545

    feat: add win conditions for the game

---
+++ lib/board.rb
@@ -0,0 +1,15 @@
+# Pre-define the winning conditions
+WIN_CONDITIONS = [
+  # Rows
+  [:a1, :a2, :a3],
+  [:b1, :b2, :b3],
+  [:c1, :c2, :c3],
+  # Columns
+  [:a1, :b1, :c1],
+  [:a2, :b2, :c2],
+  [:a3, :b3, :c3],
+  # Diagonals
+  [:a1, :b2, :c3],
+  [:a3, :b2, :c1]
+]
+
